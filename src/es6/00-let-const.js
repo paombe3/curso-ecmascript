e160c37055219d805cf6e75f558e3946a332e75a
Paola Garcia <paombe3@gmail.com>
2022-11-28 11:39:55 +0100

    11

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,28 @@
+var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+
+const fruits = () => {
+  if(true) {
+    var fruit1 = 'Apple';
+    let fruit2 = 'Kiwi';
+    const fruit3 = 'Banana';
+  }
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+}
+
+fruits ();
